7419df49a3e7530cb9b2e256125fc03295e8e2fd
Marvin <marvinkamwenjih@gmail.com>
2023-02-10 07:53:17 +0300

    finishing up on mainpage

--- src/Pages/Mainpage.jsx
+++ src/Pages/Mainpage.jsx
@@ -1,32 +1,27 @@
 import '../css/Mainpage.css';
-import profile from '../assets/profile.jpeg'
+import NavArea from './NavArea';
+
 function Mainpage (){
     return (
         <div className="container-fluid">
             <div className="row">
-                <div className="col-4 navArea">
-                    <div className="topNav">
-                     <img src={profile} className=" profileImage img-fluid" alt="profile"/>
-                      <h3 className="profileName">Marvin Kamwenji Murage</h3>
-                      <hr />
-                    </div>
-                    <div className="bottomNav">
-                        <ul className='lists'>
-                            <li className="linkLists" ><a href="#about">About</a></li>
-                            <li className="linkLists" ><a href="#skill">Skill</a></li>
-                            <li className="linkLists" ><a href="#design">Design</a></li>
-                            <li className="linkLists" ><a href="#preview">Preview</a></li>
-                            <li className="linkLists" ><a href="#contact">Contact</a></li>
-                        </ul>
-                        <div className="button-div">
-                            <button className="hire_me_button">Hire me</button>
-                        </div>
-                    </div>
-                </div>
+                <NavArea />
                 <div className="col-8 content_area">
                     <div className="content-page">
-                      <h1>World</h1>
+                      <p>I am a highly motivated and passionate individual with skills 
+                        and expertise in software development and Quality Assurance. 
+                         </p>
+
+                        <p>I care deeply about building high quality softwares and I'm 
+                        excited to put my creativity and skills to work to actively 
+                        contribute towards creating innovative solutions.</p>
+
+
                     </div>
+                    <div className="content-buttons">
+                            <button className='content_hire_me_button'>Hire me</button>
+                            <button className="contact_me_button">Contact me</button>
+                        </div>
                 </div>
             </div>
 
